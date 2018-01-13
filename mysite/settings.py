9a5bf3f27672b8fe29ac964bf0acf315852457ec
martinlarsalbert <marale@kth.se>
2018-01-13 20:02:31 +0100

    added allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n+k65va-jzq(i6=3&q5o+o^yml#s$_j=9$1=cqj%821j=rtr+-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['marale.pythonanywhere.com']
 
 
 # Application definition
